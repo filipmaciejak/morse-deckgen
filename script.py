a2572c1fca4d53df9f6fa8ec275d4675428e16af
filipmaciejak <fimaciejak@gmail.com>
2024-08-27 17:04:41 +0200

    refactor script.py

--- script.py
+++ script.py
@@ -6,18 +6,34 @@ import subprocess
 from genanki import Deck, Model, Note, Package
 
 WORD_FILE_NAME = 'words.txt'
+OUTPUT_FILE_NAME = 'output.apkg'
 
 DECK_ID = random.randrange(1 << 30, 1 << 31)
 DECK_NAME = 'Morse Code'
+DECK_ARGS = [DECK_ID, DECK_NAME]
 
 MODEL_ID = random.randrange(1 << 30, 1 << 31)
 MODEL_NAME = 'Simple Model'
+MODEL_ARGS = [MODEL_ID, MODEL_NAME]
+MODEL_KWARGS = {
+    'fields':[
+        {'name': 'Answer'},
+        {'name': 'Recording'}
+    ],
+    'templates':[
+        {
+            'name': 'Card 1',
+            'qfmt': 'What does this say?<br>{{Recording}}',
+            'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}'
+        }
+    ]
+}
 
 TEMP_DIR = '/tmp/morse-deckgen-data'
 
-charset = 'abcdefghijklmnopqrstuvwxyz0123456789'
+CHARSET = 'abcdefghijklmnopqrstuvwxyz0123456789'
 
-chardict = {
+CHARDICT = {
     'period': '.',
     'comma': ',',
     'question_mark': '?',
@@ -37,72 +53,59 @@ chardict = {
     'at': '@'
 }
 
-words = []
-with open(WORD_FILE_NAME) as word_file:
-    for line in word_file.readlines():
-        if len(line) > 1:
-            words.append(line)
 
-data = [] # (code name, input / display name, file name)
-for c in charset:
-    data.append((c, c, 'char_' + c))
-for key, value in chardict.items():
-    data.append((key, value, 'char_' + key))
-for word in words:
-    data.append((word, word, 'word_' + word))
+def load_words(filename: str) -> list[str]:
+    words = []
+    with open(filename) as file:
+        for line in file.readlines():
+            word = line.strip()
+            if len(word) > 1:
+                words.append(word)
+    return words
 
-my_deck = Deck(
-    DECK_ID,
-    DECK_NAME
-)
 
-my_model = Model(
-    MODEL_ID,
-    MODEL_NAME,
-    fields=[
-        {'name': 'Answer'},
-        {'name': 'Recording'}
-    ],
-    templates=[
-        {
-            'name': 'Card 1',
-            'qfmt': 'What does this say?<br>{{Recording}}',
-            'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}'
-        }
-    ]
-)
+def generate_audio(input:str, filename: str) -> None:
+    assert filename.endswith('.wav')
+    cmd = ['cwwav', '--output', f'{filename}']
+    subprocess.run(cmd, input=bytes(input, 'utf-8'))
 
-media_files = []
-subprocess.run(['mkdir', f'{TEMP_DIR}'])
-for note_data in data:
-    
-    code_name = note_data[0]
-    raw_text = note_data[1]
-    file_name = note_data[2]
 
-    file_path = f'{TEMP_DIR}/{file_name}'
-    
-    # generate audio file
-    subprocess.run(['cwwav', '--output', f'{file_path}.wav'], input=bytes(raw_text, 'utf-8'))
-    
-    # convert the audio from wav to mp3
-    subprocess.run(['ffmpeg', '-i', f'{file_path}.wav', f'{file_path}.mp3'])
-    
-    # add file to media_files
-    media_files.append(f'{file_path}.mp3')
-    
-    # create a note
-    note = Note(
-        model=my_model,
-        fields=[raw_text, f'[sound:{file_name}.mp3]']
-    )
-    
-    my_deck.add_note(note)
+def convert_media_file(input_filename: str, output_filename: str) -> None:
+    cmd = ['ffmpeg', '-i', input_filename, output_filename]
+    subprocess.run(cmd)
 
-my_package = Package(my_deck)
-my_package.media_files = media_files
 
-my_package.write_to_file('output.apkg')
+def main():
+    words = load_words(WORD_FILE_NAME)
+    data = [] # (word id, word text)
+    for c in CHARSET:
+        data.append(('char_' + c, c))
+    for key, value in CHARDICT.items():
+        data.append(('char_' + key, value))
+    for word in words:
+        data.append(('word_' + word, word))
+    deck = Deck(*DECK_ARGS)
+    model = Model(*MODEL_ARGS, **MODEL_KWARGS)
+    media_files = []
+    subprocess.run(['mkdir', f'{TEMP_DIR}'])
+    for note_data in data:
+        word_id = note_data[0]
+        word_text = note_data[1]
+        filepath = f'{TEMP_DIR}/{word_id}'
+        generate_audio(word_text, filepath + '.wav')
+        convert_media_file(filepath + '.wav', filepath + '.mp3')
+        media_files.append(filepath + '.mp3')
+        note = Note(
+            model=model,
+            fields=[word_text, f'[sound:{word_id}.mp3]']
+        )
+        deck.add_note(note)
+    package = Package(deck)
+    package.media_files = media_files
+    package.write_to_file(OUTPUT_FILE_NAME)
+    subprocess.run(['rm', '-rf', f'{TEMP_DIR}'])
 
-subprocess.run(['rm', '-rf', f'{TEMP_DIR}'])
+
+if __name__ == '__main__':
+    main()
 
